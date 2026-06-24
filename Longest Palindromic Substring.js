88367d098b6f51ffc02296fd3ad2d8816bf1ff87
T1 LeVi <ahmadazzeh2003@gmail.com>
2026-06-24 19:16:09 +0300

    Longest Palindromic Substring

---
+++ Longest Palindromic Substring.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if (s.length < 1) return "";
+
+    let start = 0;
+    let end = 0;
+
+    const expandFromCenter = function(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const len1 = expandFromCenter(i, i);
+        const len2 = expandFromCenter(i, i + 1);
+        const len = Math.max(len1, len2);
+
+        if (len > end - start + 1) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);
+};
