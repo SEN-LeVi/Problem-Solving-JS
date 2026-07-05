3818dfafa6188e9f543fa359ab641f4eb4e14aab
T1 LeVi <ahmadazzeh2003@gmail.com>
2026-07-05 21:52:20 +0300

    Swap Nodes in Pairs

---
+++ Swap Nodes in Pairs.js
@@ -0,0 +1,22 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    const dummy = new ListNode(0);
+    dummy.next = head;
+    let prev = dummy;
+
+    while (prev.next !== null && prev.next.next !== null) {
+        const first = prev.next;
+        const second = prev.next.next;
+
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        prev = first;
+    }
+
+    return dummy.next;
+};
