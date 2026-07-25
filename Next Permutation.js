19f6cd12ea867034f748bf9697c3c878006f0c08
T1 LeVi <ahmadazzeh2003@gmail.com>
2026-07-25 17:56:45 +0300

    Next Permutation

---
+++ Next Permutation.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    const n = nums.length;
+
+    // Step 1: find first index i (from the right) where nums[i] < nums[i+1].
+    let i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    // Step 2: if such i exists, find the rightmost value bigger than nums[i] and swap.
+    if (i >= 0) {
+        let j = n - 1;
+        while (nums[j] <= nums[i]) {
+            j--;
+        }
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    // Step 3: reverse everything after index i to make it ascending (smallest).
+    let left = i + 1;
+    let right = n - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+};
+
